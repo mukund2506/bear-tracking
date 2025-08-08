8c7e0fa9307bd17bc0824f7bd61573866854b4b7
Mukund <137892321+mukund2506@users.noreply.github.com>
2025-08-08 12:18:07 +0530

    code

---
+++ bearList.js
@@ -0,0 +1,31 @@
+import { NavigationMixin } from 'lightning/navigation';
+import { LightningElement, wire } from 'lwc';
+/** BearController.searchBears(searchTerm) Apex method */
+import searchBears from '@salesforce/apex/BearController.searchBears';
+export default class BearList extends NavigationMixin(LightningElement) {
+	searchTerm = '';
+	@wire(searchBears, {searchTerm: '$searchTerm'})
+	bears;
+	handleSearchTermChange(event) {
+
+		window.clearTimeout(this.delayTimeout);
+		const searchTerm = event.target.value;
+		this.delayTimeout = setTimeout(() => {
+			this.searchTerm = searchTerm;
+		}, 300);
+	}
+	get hasResults() {
+		return (this.bears.data.length > 0);
+	}
+	handleBearView(event) {
+		const bearId = event.detail;
+		this[NavigationMixin.Navigate]({
+			type: 'standard__recordPage',
+			attributes: {
+				recordId: bearId,
+				objectApiName: 'Bear__c',
+				actionName: 'view',
+			},
+		});
+	}
+}
